1b3e43a9e59791c7ec0bb6ea951a146fe391cdb7
Annie Jeevas <Annie Jeevas>
2016-09-27 20:41:56 +0300

    first commit

---
+++ markov_chains/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace markov_chains
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dataGridView4 = new System.Windows.Forms.DataGridView();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(233, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1. Укажите количество состояний";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.comboBox1.Location = new System.Drawing.Point(251, 6);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Text = "5";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 75);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(493, 452);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 17);
+            this.label2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(378, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "ОК";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(661, 202);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(204, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Рассчитать вектор";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(555, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(411, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "4. Укажите момент времени для расчета вероятностей цепи";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(709, 174);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(113, 22);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "10";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(558, 260);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(483, 80);
+            this.dataGridView2.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(555, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(228, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "3. Укажите начальное состояние";
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(558, 51);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowTemplate.Height = 24;
+            this.dataGridView3.Size = new System.Drawing.Size(483, 80);
+            this.dataGridView3.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(555, 240);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(196, 17);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Статистические результаты";
+            // 
+            // dataGridView4
+            // 
+            this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView4.Location = new System.Drawing.Point(558, 379);
+            this.dataGridView4.Name = "dataGridView4";
+            this.dataGridView4.RowTemplate.Height = 24;
+            this.dataGridView4.Size = new System.Drawing.Size(483, 89);
+            this.dataGridView4.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(555, 359);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(191, 17);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Аналитические результаты";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(555, 471);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(135, 17);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "*согласно формуле";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(696, 477);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(345, 61);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(554, 523);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1077, 572);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dataGridView4);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Расчет цепи Маркова";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dataGridView4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ markov_chains/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace markov_chains
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private String TextLabel2 = "2. Заполните таблицу вероятностей переходов";
+        int n; //колво состояний
+        int[] kolvo;
+        Random r = new Random();
+        int constK = 10000;
+        int time;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label2.Text = TextLabel2;
+            n = Int32.Parse(comboBox1.Text);
+            dataGridView1.ColumnCount = n;
+            dataGridView1.RowCount = n;
+            dataGridView3.ColumnCount = n;
+            dataGridView3.RowCount = 1;
+            dataGridView3.Rows[0].HeaderCell.Value = "P0";
+            for (int i = 0; i<n;i++)
+            {
+                dataGridView1.Columns[i].Width = 30;
+                dataGridView3.Columns[i].Width = 30;
+                dataGridView1.Columns[i].HeaderText = i.ToString();                
+                dataGridView3.Columns[i].HeaderText = "S" + i.ToString();
+                dataGridView1.Rows[i].HeaderCell.Value = i.ToString();
+                dataGridView3.Rows[0].Cells[i].Value = 0;
+                for (int j = 0; j < n; j++)
+                { dataGridView1.Rows[i].Cells[j].Value = 0; }
+
+                dataGridView1.Rows[i].Cells[i].Value = 0;
+            }
+            dataGridView3.Rows[0].Cells[0].Value = 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView2.ColumnCount = n;
+            dataGridView2.RowCount = 1;
+            dataGridView2.Rows[0].HeaderCell.Value = "P";
+            dataGridView4.ColumnCount = n;
+            dataGridView4.RowCount = 1;
+            dataGridView4.Rows[0].HeaderCell.Value = "P";
+            for (int i = 0; i < n; i++)
+            {
+                dataGridView2.Columns[i].Width = 30;
+                dataGridView2.Columns[i].HeaderText = "S"+i.ToString();
+                dataGridView4.Columns[i].Width = 30;
+                dataGridView4.Columns[i].HeaderText = "S" + i.ToString();
+            }
+            time = Int32.Parse(textBox1.Text);
+
+            /////////////////////
+            //метод монте-карло//
+            /////////////////////
+            kolvo = new int[n];           
+            for (int k = 0; k < constK; k++)
+            {
+                int tekushee = -1;
+                //выбираем текущее из начальных
+                int rand = r.Next(1,100);
+                int now = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    int a = Int32.Parse(dataGridView3.Rows[0].Cells[i].Value.ToString()); //текущая ячейка
+                    if ((rand > now)&&(rand <= now+a))
+                            { tekushee = i; break; }
+                    now += a;
+                }
+                if (tekushee == -1) throw new Exception();
+                now = 0;
+                for (int i = 0; i < time; i++)
+                {
+                    rand = r.Next(1,100);
+                    for (int j = 0; j < n; j++)
+                    {
+                        int a = Int32.Parse(dataGridView1.Rows[tekushee].Cells[j].Value.ToString()); //текущая ячейка
+                        if ((rand > now) && (rand < now + a))
+                        { tekushee = j; break; }
+                        now += a;
+                    }
+                    //выбираем куда идти из текущего положения
+                    now = 0;
+                }
+                kolvo[tekushee]+= 1;
+                tekushee = -1;
+            }
+
+            for (int i = 0; i < n; i++)
+            { dataGridView2.Rows[0].Cells[i].Value = (double)kolvo[i]*100/constK;
+                dataGridView2.Columns[i].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+
+            }
+
+           
+        }
+
+        private double P(int i, int time)
+        {
+           
+            if (time == 0) return double.Parse(dataGridView3.Rows[0].Cells[i].Value.ToString())/100;
+            else
+            {
+                double sum = 0;
+                for (int j = 0; j < n; j++)
+                { sum += P(j, time - 1) * double.Parse(dataGridView1.Rows[j].Cells[i].Value.ToString())/100; }
+                return sum;
+            } 
+            
+
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ///////////////////////
+            //аналитический метод//
+            ///////////////////////
+            
+            for (int i = 0; i < n; i++)
+            {
+                dataGridView4.Rows[0].Cells[i].Value = (100* P(i, time)).ToString("##.##");
+                dataGridView4.Columns[i].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            }
+        }
+    }
+}
